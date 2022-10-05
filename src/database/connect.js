278661499dd01383b3b7d42d796ee5e80e29016f
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-10-05 16:05:40 -0300

    update

--- src/database/connect.js
+++ src/database/connect.js
@@ -5,7 +5,7 @@ const connectToDatabase = async () => {
     `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cursonode.wiqwnh8.mongodb.net/?retryWrites=true&w=majority`,
     (err) => {
       if (err) {
-        return console.log("Erro ao conectar ao banco");
+        return console.log(err.message);
       }
 
       return console.log("Conexão estabelecida com sucesso!");
